b7b22f66ca44600cf33df5c78462babd2482f147
Manish Sonawane <152314552+ManishSonawane24@users.noreply.github.com>
2024-08-24 08:01:37 +0000

    new file added

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 1234
+let accountMail = "manishsonawane2424@gmail.com"
+var accountPass = "7396"// prefer not to use var because of its scope issue
+accountCity = "Chopda"// also a method to reserve mem =ory for variable but not preferable
+
+// accountId = 2
+accountMail = "newmail@gmail.com"
+accountPass = 12345678
+accountCity = "Galwade"
+let accountState
+
+console.log(accountId);
+console.table([accountId,accountMail,accountPass,accountCity,accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,10 @@
+"use strict"
+let name = "Manish"
+console.log(typeof name);
+
+let age = 18
+console.log(typeof age);
+let loggedIn = true;
+console.log(typeof loggedIn);
+
+// 
